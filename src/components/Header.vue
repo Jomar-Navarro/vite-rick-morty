17fb5f281f97d886330a793de4dfb4fafe5c08da
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-03-25 17:37:38 +0100

    [add] inputs and buttons

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,8 +11,20 @@
 
 
 <template>
-  <div class="text-center my-3">
+  <div class="text-center my-3 container">
     <h1 class="text-white">{{ store.mainTitle }}</h1>
+    <div class="d-flex my-5 justify-content-center">
+      <input type="text" class="form-control w-25" placeholder="Search Character">
+      <select class="form-select mx-3 w-25">
+        <option selected>Select status</option>
+        <option value="1">Alive</option>
+        <option value="2">Dead</option>
+        <option value="3">Unknown</option>
+      </select>
+      <button class="btn btn-success">Search</button>
+      <button class="btn btn-danger mx-3">Reset</button>
+    </div>
+
   </div>
 </template>
 

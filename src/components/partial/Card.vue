47d97688af05afd601a01b017f7f632899370167
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-03-25 17:17:28 +0100

    [add] style

--- src/components/partial/Card.vue
+++ src/components/partial/Card.vue
@@ -12,7 +12,7 @@
 
 <template>
   <div class="card" style="width: 18rem;">
-  <img :src="image" class="card-img-top" alt="...">
+  <img :src="image" class="card-img-top p-3" alt="...">
   <div class="card-body">
     <h5 class="card-title">{{ name }}</h5>
     <hr>
@@ -24,5 +24,11 @@
 
 
 <style lang="scss" scoped>
-
+  .card{
+    border-radius: 100%;
+    box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
+      img{
+        border-radius: 50%;
+      }
+  }
 </style>

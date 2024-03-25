c299f7b7f3ac3f789f467ff5f55c85a3bc0f1095
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-03-25 16:28:01 +0100

    [add] store and app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,43 @@
 <script>
+  import axios from 'axios';
+  import { store } from './data/store';
   import Header from './components/Header.vue';
   import Main from './components/Main.vue';
   import Footer from './components/Footer.vue';
   export default {
+    data(){
+      return{
+        store,
+      }
+    },
+
     components:{
       Header,
       Main,
       Footer,
+    },
+
+    methods:{
+      getApi(){
+        axios.get(this.store.apiUrl, {
+          params:{
+            num: 20,
+            offset: 0,
+            
+          }
+        })
+      .then(result => {
+        this.store.cardsList = result.data.data;
+        console.log(this.store.cardsList);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+      }
+    },
+
+    mounted(){
+      this.getApi()
     }
   }
 </script>

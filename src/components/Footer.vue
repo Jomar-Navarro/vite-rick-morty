c8aa81d5b74cdf5837210f105273f4d0ba9b8801
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-04-03 10:55:43 +0200

    [add] result card found

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,18 @@
 <script>
+  import { store} from '../data/store'
   export default {
-    
+    data(){
+      return{
+        store
+      }
+    }
   }
 </script>
 
 
 <template>
   <div class="text-center">
+    <p>Risultati trovati: {{ store.cardsList.length }}</p>
   </div>
 </template>
 
